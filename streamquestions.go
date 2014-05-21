e0fab34b68b1cb072a121454ecbbfb1b59c643a5
MasterPpv <pritenvora@gmail.com>
2014-05-21 08:17:20 -0500

    Have a basic UI for prompting users for a query to search for, and basic Twitter functionality. Need to work with queues now and get things displaying properly as well as adding basic functionality.

--- streamquestions.go
+++ streamquestions.go
@@ -1,54 +1,157 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
 	"fmt"
-	//"github.com/ChimeraCoder/anaconda"
+	"io/ioutil"
+	"log"
+	"net/url"
+	"os"
+	//"sync"
+	//"strings"
+	"github.com/garyburd/go-oauth/oauth"
+	"github.com/garyburd/twitterstream"
+	"github.com/ChimeraCoder/anaconda"
+	"github.com/nsf/termbox-go"
 )
 
+var (
+	configPath = flag.String("config", "config.json", "Path to configuration file containing the application's credentials.")
+
+	accessToken oauth.Credentials
+
+	oauthClient = oauth.Client {
+		TemporaryCredentialRequestURI: "https://api.twitter.com/oauth/request_token",
+		ResourceOwnerAuthorizationURI: "https://api.twitter.com/oauth/authorize",
+		TokenRequestURI:               "https://api.twitter.com/oauth/access_token",
+	}
+)
+
+func readConfig() error {
+	b, err := ioutil.ReadFile(*configPath)
+	if err != nil {
+		return err
+	}
+	var config = struct {
+		Consumer, Access *oauth.Credentials
+	}{
+		&oauthClient.Credentials, &accessToken,
+	}
+	return json.Unmarshal(b, &config)
+}
+
+func usage() {
+	fmt.Fprintf(os.Stderr, "Usage of %s:\n %s keyword ...\n", os.Args[0], os.Args[0])
+	flag.PrintDefaults()
+	os.Exit(2)
+}
+
 func main() {
-	x, y, z := 5, 10, 15
-	myQueue := new(Queue)
-	fmt.Println("Size before calling Init:", myQueue.GetSize())
-	fmt.Println("IsEmpty before calling Init:", myQueue.IsEmpty())
-	myQueue = myQueue.Init()
-	fmt.Println("After calling Init:", *myQueue)
-	fmt.Println("Size after calling Init:", myQueue.GetSize())
-	fmt.Println("IsEmpty after calling Init:", myQueue.IsEmpty())
-	myQueue.Enqueue(x)
-	fmt.Println("After calling Enqueue once:", *myQueue)
-	fmt.Println("Head after calling Enqueue once:", myQueue.head)
-	fmt.Println("Tail after calling Enqueue once:", myQueue.tail)
-	fmt.Println("Size after calling Enqueue once:", myQueue.GetSize())
-	fmt.Println("IsEmpty after calling Enqueue once:", myQueue.IsEmpty())
-	myQueue.Enqueue(y)
-	fmt.Println("After calling Enqueue twice:", *myQueue)
-	fmt.Println("Head after calling Enqueue twice:", myQueue.head)
-	fmt.Println("Tail after calling Enqueue twice:", myQueue.tail)
-	fmt.Println("Size after calling Enqueue twice:", myQueue.GetSize())
-	fmt.Println("IsEmpty after calling Enqueue twice:", myQueue.IsEmpty())
-	myQueue.Enqueue(z)
-	fmt.Println("After calling Enqueue three times:", *myQueue)
-	fmt.Println("Head after calling Enqueue three times:", myQueue.head)
-	fmt.Println("Tail after calling Enqueue three times:", myQueue.tail)
-	fmt.Println("Size after calling Enqueue three times:", myQueue.GetSize())
-	fmt.Println("IsEmpty after calling Enqueue three times:", myQueue.IsEmpty())
-	fmt.Println("Top after calling Enqueue three times:", myQueue.Top())
-	fmt.Println("Dequeueing:", myQueue.Dequeue())
-	fmt.Println("Top after calling Dequeue once:", myQueue.Top())
-	fmt.Println("Head after calling Dequeue once:", myQueue.head)
-	fmt.Println("Tail after calling Dequeue once:", myQueue.tail)
-	fmt.Println("Size after calling Dequeue once:", myQueue.GetSize())
-	fmt.Println("IsEmpty after calling Dequeue once:", myQueue.IsEmpty())
-	fmt.Println("Dequeueing:", myQueue.Dequeue())
-	fmt.Println("Top after calling Dequeue twice:", myQueue.Top())
-	fmt.Println("Head after calling Dequeue twice:", myQueue.head)
-	fmt.Println("Tail after calling Dequeue twice:", myQueue.tail)
-	fmt.Println("Size after calling Dequeue twice:", myQueue.GetSize())
-	fmt.Println("IsEmpty after calling Dequeue twice:", myQueue.IsEmpty())
-	fmt.Println("Dequeueing:", myQueue.Dequeue())
-	fmt.Println("Top after calling Dequeue three times:", myQueue.Top())
-	fmt.Println("Head after calling Dequeue three times:", myQueue.head)
-	fmt.Println("Tail after calling Dequeue three times:", myQueue.tail)
-	fmt.Println("Size after calling Dequeue three times:", myQueue.GetSize())
-	fmt.Println("IsEmpty after calling Dequeue three times:", myQueue.IsEmpty())
+	//queryMutex := new(sync.Mutex)
+	//queryMutex.Lock()
+	termbox_err := termbox.Init()
+	if termbox_err != nil {
+		panic(termbox_err)
+	}
+	defer termbox.Close()
+	termbox.SetInputMode(termbox.InputEsc)
+	edit_box.InsertRune('#')
+
+	redraw_all()
+	query := ""
+	stringSlices := make([]rune, 0)
+	stringSlices = append(stringSlices, '#')
+	queryloop: for {
+		switch ev := termbox.PollEvent(); ev.Type {
+		case termbox.EventKey:
+			switch ev.Key {
+			case termbox.KeyEsc:
+				break queryloop
+			case termbox.KeySpace:
+				edit_box.InsertRune(' ')
+				stringSlices = append(stringSlices, ' ')
+			case termbox.KeyEnter:
+				query = string(stringSlices)
+				break queryloop
+			case termbox.KeyBackspace, termbox.KeyBackspace2:
+				edit_box.DeleteRuneBackward()
+				if len(stringSlices) > 0 {
+					tempSlices := make([]rune, len(stringSlices) - 1)
+					copy(tempSlices, stringSlices[0:])
+					stringSlices[len(stringSlices) - 1] = 0
+					stringSlices = tempSlices
+				}
+			default:
+				if ev.Ch != 0 {
+					edit_box.InsertRune(ev.Ch)
+					stringSlices = append(stringSlices, ev.Ch)
+				}
+			}
+		case termbox.EventError:
+			panic(ev.Err)
+		}
+		redraw_all()
+	}
+	if query == "" || query == "#" || query == " " || query == "# " {
+		fmt.Println("No query given. Nothing to search for. Program exiting...")
+		//queryMutex.Unlock()
+		return
+	} else {
+		termbox.Close()
+		fmt.Print("Searching for: ", query, "\n")
+		fmt.Println()
+		//queryMutex.Unlock()
+	}
+
+	displayQueue := new(Queue)
+	displayQueue = displayQueue.Init()
+	newTweetsQueue := new(Queue)
+	newTweetsQueue = newTweetsQueue.Init()
+	oldTweetsQueue := new(Queue)
+	oldTweetsQueue = oldTweetsQueue.Init()
+
+	flag.Usage = usage
+	flag.Parse()
+	if err := readConfig(); err != nil {
+		log.Fatalf("Error reading configuration, %v", err)
+	}
+
+	ts, err := twitterstream.Open(
+		&oauthClient,
+		&accessToken,
+		"https://stream.twitter.com/1.1/statuses/filter.json",
+		//url.Values{"track": {strings.Join(flag.Args(), ", ")}})
+		url.Values{"track": {query}})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ts.Close()
+
+	// Loop until stream has a permanent error.
+	for ts.Err() == nil {
+		var t anaconda.Tweet
+		if err := ts.UnmarshalNext(&t); err != nil {
+			log.Fatal(err)
+		}
+		displaySize := displayQueue.GetSize()
+		if displaySize < 10 {
+			displayQueue.Enqueue(t)
+			fmt.Print("@", t.User.ScreenName, ": ", t.Text, " (https://twitter.com/", t.User.ScreenName, "/status/", t.Id,")")
+			fmt.Println()
+			fmt.Println()
+		} else {
+			newTweetsSize := newTweetsQueue.GetSize()
+			if newTweetsSize == 0 {
+				fmt.Println("Display queue filled.")
+				fmt.Println("Now filling new Tweets queue.")
+				fmt.Println()
+			}
+			newTweetsQueue.Enqueue(t)
+			fmt.Print("@", t.User.ScreenName, ": ", t.Text, " (https://twitter.com/", t.User.ScreenName, "/status/", t.Id,")")
+			fmt.Println()
+			fmt.Println()
+		}
+	}
+	log.Print(ts.Err)
 }
